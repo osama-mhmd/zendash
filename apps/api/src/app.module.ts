6205855e17d10a13044987d04bcc8413ccb1992c
osama-mhmd <docosmo010@gmail.com>
2026-01-23 16:55:06 +0200

    fix: Stupid nestjs

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "@modules/auth/auth.module";
-import { ProjectsModule } from "./projects/projects.module";
-import { EventsModule } from "./events/events.module";
+import { ProjectsModule } from "@modules/projects/projects.module";
+import { EventsModule } from "@modules/events/events.module";
 
 @Module({
   imports: [AuthModule, ProjectsModule, EventsModule],
