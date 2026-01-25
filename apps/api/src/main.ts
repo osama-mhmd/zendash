e1bf871480fe7119d4f88de3afebbea273c66e8b
osama-mhmd <docosmo010@gmail.com>
2026-01-25 11:49:14 +0200

    feat(api): allowing any origin to send requests without credentials

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,16 +2,34 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import cookieParser from "cookie-parser";
+import { Request } from "express";
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-  app.enableCors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  });
+  app.enableCors(
+    (
+      req: Request,
+      callback: (error: null | Error, options: CorsOptions) => void,
+    ) => {
+      const origin = req.header("Origin");
+
+      if (origin == process.env.FRONTEND_URL) {
+        callback(null, {
+          origin,
+          credentials: true,
+        });
+      } else {
+        callback(null, {
+          origin: "*",
+          credentials: false,
+        });
+      }
+    },
+  );
 
   await app.listen(process.env.PORT ?? 4000);
 }
