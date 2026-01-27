f91a5839ff5d39d88cafaf1fbd9cfd974bd403b0
osama-mhmd <docosmo010@gmail.com>
2026-01-27 16:45:31 +0200

    fix(api): This is so funny

--- apps/api/src/modules/issues/issues.controller.ts
+++ apps/api/src/modules/issues/issues.controller.ts
@@ -10,18 +10,18 @@ export class IssuesController {
   constructor(private readonly issuesService: IssuesService) {}
 
   @UseGuards(AuthGuard)
-  @Get(":id")
-  async get(@Req() req: Request, @Param("id") id: string) {
-    const issue = await this.issuesService.get(id);
-
-    return { ok: true, data: issue };
-  }
-
-  @UseGuards(AuthGuard)
   @Get("me")
   async me(@Req() req: Request) {
     const issues = await this.issuesService.me(req.user!.id);
 
     return { ok: true, data: issues };
+  }
+
+  @UseGuards(AuthGuard)
+  @Get(":id")
+  async get(@Req() req: Request, @Param("id") id: string) {
+    const issue = await this.issuesService.get(id);
+
+    return { ok: true, data: issue };
   }
 }
